f90b72af323e47088ea47f9fd17d4e13cb8962f0
gustavo_dev <gustavo.isaac@gmail.com>
2023-05-18 11:34:22 +0100

    Compose dependencies

---
+++ buildSrc/src/main/java/Compose.kt
@@ -0,0 +1,12 @@
+object Compose {
+
+    private const val activityComposeVersion = "1.5.1" //1.7.1
+    const val activityCompose = "androidx.activity:activity-compose:$activityComposeVersion"
+
+    private const val composeBomVersion = "2022.10.00" //mudar para o mais recente
+    const val composeBom = "androidx.compose:compose-bom:$composeBomVersion"
+    const val ui = "androidx.compose.ui:ui"
+    const val uiGraphics = "androidx.compose.ui:ui-graphics"
+    const val uiToolingPreview = "androidx.compose.ui:ui-tooling-preview"
+    const val material3 = "androidx.compose.material3:material3"
+}
