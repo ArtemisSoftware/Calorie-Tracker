a4c4df3cbe7e19b1f4323cd18a6cb930125f0f2f
gustavo_dev <gustavo.isaac@gmail.com>
2023-05-18 11:15:15 +0100

    AndroidX dependencies

---
+++ buildSrc/src/main/java/AndroidX.kt
@@ -0,0 +1,8 @@
+object AndroidX {
+
+    private const val coreKtxVersion = "1.8.0" //1.10.1
+    const val coreKtx = "androidx.core:core-ktx:$coreKtxVersion"
+
+    private const val lifecycleRuntimeKtxVersion = "2.3.1" //2.6.1
+    const val lifecycleRuntimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleRuntimeKtxVersion"
+}
