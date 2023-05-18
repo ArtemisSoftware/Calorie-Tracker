e73c499e56405b40a1daf644e3d131f730b0e77a
gustavo_dev <gustavo.isaac@gmail.com>
2023-05-18 11:46:35 +0100

    Testing dependencies

---
+++ buildSrc/src/main/java/Testing.kt
@@ -0,0 +1,15 @@
+object Testing {
+
+    private const val junitVersion = "4.13.2"
+    const val junit = "junit:junit:$junitVersion"
+
+    private const val extJunitVersion = "1.1.3" //1.1.5
+    const val extJunit = "androidx.test.ext:junit:$extJunitVersion"
+
+    private const val espressoCoreVersion = "3.4.0" //3.5.1
+    const val espressoCore = "androidx.test.espresso:espresso-core:$espressoCoreVersion"
+
+    const val composeUiTestJunit4 = "androidx.compose.ui:ui-test-junit4"
+    const val composeUiTooling = "androidx.compose.ui:ui-tooling"
+    const val composeUiTestManifest = "androidx.compose.ui:ui-test-manifest"
+}
