542452db18ceef3c7b66278527c58377a0476bd8
gustavo_dev <gustavo.isaac@gmail.com>
2023-05-19 06:47:56 +0100

    compose-module.gradle

--- buildSrc/src/main/java/Compose.kt
+++ buildSrc/src/main/java/Compose.kt
@@ -3,6 +3,8 @@ object Compose {
     private const val activityComposeVersion = "1.5.1" //1.7.1
     const val activityCompose = "androidx.activity:activity-compose:$activityComposeVersion"
 
+    const val compilerVersion = "1.3.2"
+
     private const val composeBomVersion = "2022.10.00" //mudar para o mais recente
     const val composeBom = "androidx.compose:compose-bom:$composeBomVersion"
     const val ui = "androidx.compose.ui:ui"
