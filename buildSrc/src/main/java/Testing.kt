35a8a3f87b7731a68a7c9fe43db8a00b11465dc3
gustavo_dev <gustavo.isaac@gmail.com>
2023-06-21 10:09:04 +0100

    CalculateMealNutrientsTest

--- buildSrc/src/main/java/Testing.kt
+++ buildSrc/src/main/java/Testing.kt
@@ -12,4 +12,28 @@ object Testing {
     const val composeUiTestJunit4 = "androidx.compose.ui:ui-test-junit4"
     const val composeUiTooling = "androidx.compose.ui:ui-tooling"
     const val composeUiTestManifest = "androidx.compose.ui:ui-test-manifest"
+
+
+
+    private const val coroutinesTestVersion = "1.5.1"
+    const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesTestVersion"
+
+    private const val truthVersion = "1.1.3"
+    const val truth = "com.google.truth:truth:$truthVersion"
+
+    private const val mockkVersion = "1.10.0"
+    const val mockk = "io.mockk:mockk:$mockkVersion"
+    const val mockkAndroid = "io.mockk:mockk-android:$mockkVersion"
+
+    private const val turbineVersion = "0.7.0"
+    const val turbine = "app.cash.turbine:turbine:$turbineVersion"
+
+    private const val mockWebServerVersion = "4.9.3"
+    const val mockWebServer = "com.squareup.okhttp3:mockwebserver:$mockWebServerVersion"
+
+
+    const val hiltTesting = "com.google.dagger:hilt-android-testing:${DaggerHilt.version}"
+
+    private const val testRunnerVersion = "1.4.0"
+    const val testRunner = "androidx.test:runner:$testRunnerVersion"
 }
